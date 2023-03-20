baf6d18282e838f1a83661f64a1dcc4d063f7aef
张爽 <zhangshuang@hualala.com>
2023-03-20 18:11:00 +0800

    Initial commit

---
+++ zzj-oauth2/src/main/java/cn/zzj/oauth2/App.java
@@ -0,0 +1,13 @@
+package cn.zzj.oauth2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
