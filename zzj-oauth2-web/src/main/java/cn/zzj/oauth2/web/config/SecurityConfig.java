1d4460a89d649104256f05d1b563971bd55a2503
张爽 <zhangshuang@hualala.com>
2023-04-03 12:23:54 +0800

    init

--- zzj-oauth2-web/src/main/java/cn/zzj/oauth2/web/config/SecurityConfig.java
+++ zzj-oauth2-web/src/main/java/cn/zzj/oauth2/web/config/SecurityConfig.java
@@ -1,21 +1,34 @@
 package cn.zzj.oauth2.web.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * h2 数据库利用 spring 的内嵌的网页版查看数据，由于 spring security 的缘故，此处对其做 frame 的不校验
+ */
 @Slf4j
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        log.info("设置httpSecurity:认证方式，除注册地址以外，其它授权请求.");
+    @Value("${spring.h2.console.path}")
+    private String h2Path;
 
-        // to enable h2 web console
-        httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf().disable()
+                .headers().disable()
+                .build();
     }
 
 }
